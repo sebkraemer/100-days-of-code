bb7800b202bd27ae2c8a745a3e6a6baf6e29c02e
Sebastian Krämer <sebkraemer@posteo.org>
2021-12-24 22:42:34 +0100

    day 44: add a goroutine workers program to experiment with done pattern and mplicit cancellation

---
+++ day044/channels.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func runWorkers(workers []func() int) int {
+	done := make(chan struct{})
+	result := make(chan int)
+
+	if len(workers) < 1 {
+		return 0 // func will deadlock without workers if we don't return here
+	}
+
+	for i, worker := range workers {
+		fmt.Println("spawning worker", i)
+		go func(worker func() int) {
+			select {
+			case result <- worker():
+			case <-done:
+			}
+		}(worker)
+	}
+	res := <-result
+
+	return res
+}
+
+func main() {
+	workers := make([]func() int, 10)
+
+	for i := range workers {
+		i := i // beware of unwanted closure capturing of i!
+		fmt.Println(i)
+		workers[i] = func() int {
+			duration := time.Duration((1+i)*300) * time.Millisecond
+			fmt.Println("worker with duration", duration)
+			time.Sleep(duration)
+			return i
+		}
+	}
+
+	start := time.Now()
+	res := runWorkers(workers)
+	stop := time.Now()
+
+	fmt.Println("elapsed:", (stop.Sub(start)))
+
+	// 0 will be printed since the function will always return after the shorted worker with index 0
+	fmt.Println(res)
+}
---
+++ day044/go.mod
@@ -0,0 +1,3 @@
+module sebkraemer/100-days-of-code/day044
+
+go 1.17
